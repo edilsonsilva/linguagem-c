33b216b0159f6a87a2c040b4cb56061debabda6e
Edilson Silva <edilson_js@yahoo.com.br>
2025-09-09 16:51:40 -0300

    Fix:Correção da entrada de dados para a forma de pagamento

--- src/pagamento.c
+++ src/pagamento.c
@@ -1,35 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
+int main() {
     system("clear");
+
     float preco, resultado;
-    char forma_pg = '';
-    printf("Digite o valor a ser pago e tecle enter\n");
-    scanf("%f",&preco);
+    char forma_pg;
 
-    printf("Digita a forma de pagamento, sendo:\nc->crédito\nd->débito\nD->dinheiro\np->pix\n");
-    scanf("%c",forma_pg);
-    
+    printf("Digite o valor a ser pago e tecle enter:\n");
+    scanf("%f", &preco);
 
-    if( forma_pg == 'c'){
-        resultado = preco * (0.05 + 1);
-        // resultado = preco * 0.05 + preco;
-        printf("O valor a ser pago no crédito é %f\n",resultado);
-    }
-    else if( forma_pg == 'd'){
-        printf("O valor a ser pago no débito é %f\n",preco);
-    }
-    else if( forma_pg == 'D'){
+    // Limpar o buffer do teclado antes de ler o caractere
+    // Para a execução anterior e segue para a proxima etapa
+    getchar(); // importante para evitar que o '\n' da 
+    // leitura anterior seja capturado
+
+    printf("Digite a forma de pagamento, sendo:\n");
+    printf("c -> crédito\nd -> débito\nD -> dinheiro\np -> pix\n");
+    scanf("%c", &forma_pg);
+
+    if (forma_pg == 'c') {
+        resultado = preco * 1.05;
+        printf("O valor a ser pago no crédito é %.2f\n", resultado);
+    } else if (forma_pg == 'd') {
+        printf("O valor a ser pago no débito é %.2f\n", preco);
+    } else if (forma_pg == 'D') {
         resultado = preco * 0.98;
-        printf("O valor a ser pago no dinheiro é %f\n",resultado);
-    }
-    else if( forma_pg == 'p'){
+        printf("O valor a ser pago no dinheiro é %.2f\n", resultado);
+    } else if (forma_pg == 'p') {
         resultado = preco * 0.96;
-        printf("O valor a ser pago no pix é %f\n",resultado);
-    }
-    else{
+        printf("O valor a ser pago no pix é %.2f\n", resultado);
+    } else {
         printf("Esta forma de pagamento NÃO EXISTE!\n");
     }
+
     return 0;
 }
